2fef445885a4a29b5101ef0e299c6ea4de3b8861
Thomas Zorroche <thomas.zorroche@gmail.com>
2021-06-17 14:47:46 +0200

    [software] retrieve mesh and dense point cloud from parameters

--- src/software/pipeline/main_densePointCloudFiltering.cpp
+++ src/software/pipeline/main_densePointCloudFiltering.cpp
@@ -1,6 +1,18 @@
 #include <aliceVision/system/cmdline.hpp>
 #include <aliceVision/system/main.hpp>
 
+#include <aliceVision/fuseCut/DelaunayGraphCut.hpp>
+#include <aliceVision/mesh/Texturing.hpp>
+#include <aliceVision/mesh/Mesh.hpp>
+#include <aliceVision/sfmData/SfMData.hpp>
+#include <aliceVision/sfmDataIO/sfmDataIO.hpp>
+
+#include <OpenMesh/Core/IO/reader/OBJReader.hh>
+#include <OpenMesh/Core/IO/writer/OBJWriter.hh>
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 
@@ -24,7 +36,7 @@ int aliceVision_main(int argc, char** argv)
 {
     // command-line parameters
     std::string verboseLevel = system::EVerboseLevel_enumToString(system::Logger::getDefaultVerboseLevel());
-    std::string rawSfmDataPath;
+    std::string densePointCloudPath;
     std::string outputMeshPath;
     std::string inputMeshPath;
 
@@ -32,21 +44,81 @@ int aliceVision_main(int argc, char** argv)
 
     po::options_description requiredParams("Required parameters");
     requiredParams.add_options()
-        ("input,i", po::value<std::string>(&rawSfmDataPath)->required(), 
+        ("inputRawSfm,i", po::value<std::string>(&densePointCloudPath)->required(), 
             "SfMData file.")
         ("inputMesh,i", po::value<std::string>(&inputMeshPath)->required(), 
             "Input Mesh (OBJ file format).")(
         "outputMesh,o", po::value<std::string>(&outputMeshPath)->required(), 
             "Output mesh (OBJ file format).");
 
+    po::options_description optionalParams("Optional parameters");
+
     po::options_description logParams("Log parameters");
-    logParams.add_options()("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
-                            "verbosity level (fatal,  error, warning, info, debug, trace).");
+    logParams.add_options()
+        ("verboseLevel,v", po::value<std::string>(&verboseLevel)->default_value(verboseLevel),
+            "verbosity level (fatal,  error, warning, info, debug, trace).");
+
+    allParams.add(requiredParams).add(optionalParams).add(logParams);
+
+    po::variables_map vm;
+
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, allParams), vm);
+
+        if(vm.count("help") || (argc == 1))
+        {
+            ALICEVISION_COUT(allParams);
+            return EXIT_SUCCESS;
+        }
+
+        po::notify(vm);
+    }
+    catch(boost::program_options::required_option& e)
+    {
+        ALICEVISION_CERR("ERROR: " << e.what() << std::endl);
+        ALICEVISION_COUT("Usage:\n\n" << allParams);
+        return EXIT_FAILURE;
+    }
+    catch(boost::program_options::error& e)
+    {
+        ALICEVISION_CERR("ERROR: " << e.what() << std::endl);
+        ALICEVISION_COUT("Usage:\n\n" << allParams);
+        return EXIT_FAILURE;
+    }
+
+    ALICEVISION_COUT("Program called with the following parameters:");
+    ALICEVISION_COUT(vm);
+
+    // set verbose level
+    system::Logger::get()->setLogLevel(verboseLevel);
 
 
+    /////////////////////////////////////////////////////////////////
+    // Dense Point Cloud Filtering
 
+    ALICEVISION_COUT("DENSE POINT CLOUD FILTERING BEGIN...");
 
-    ALICEVISION_COUT("DENSE POINT CLOUD FILTERING DEBUG...");
+    // Mesh type
+    typedef OpenMesh::TriMesh_ArrayKernelT<> Mesh;
+
+    // Retrieve Mesh
+    Mesh mesh;
+    if(!OpenMesh::IO::read_mesh(mesh, inputMeshPath.c_str()))
+    {
+        ALICEVISION_LOG_ERROR("Unable to read input mesh from the file: " << inputMeshPath);
+        return EXIT_FAILURE;
+    }
+
+    // Retrieve Dense Sfm Data Raw
+    sfmData::SfMData densePointCloud;
+    if(!sfmDataIO::Load(densePointCloud, densePointCloudPath, sfmDataIO::ESfMData::ALL))
+    {
+        ALICEVISION_LOG_ERROR("The input SfMData file '" << densePointCloudPath << "' cannot be read.");
+        return EXIT_FAILURE;
+    }
+
+    ALICEVISION_COUT("EXIT SUCCESS");
 
     return EXIT_SUCCESS;
 }
